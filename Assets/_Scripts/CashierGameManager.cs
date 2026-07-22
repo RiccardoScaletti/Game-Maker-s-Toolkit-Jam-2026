bc6d6229c6c712d69c30a77ffd0d81bf272d2cf3
Riccardo Scaletti <rickgamedeveloper@gmail.com>
2026-07-22 17:39:15 -0500

    working game
    Minigame works, there's a bug when  there are multiple clients.

--- Assets/_Scripts/CashierGameManager.cs
+++ Assets/_Scripts/CashierGameManager.cs
@@ -1,33 +1,85 @@
 using TMPro;
-using UnityEditor.PackageManager;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CashierGameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] cashierButtons;
-    [SerializeField] private TextMeshProUGUI text;
+    public static CashierGameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    [SerializeField] private TextMeshProUGUI Cashiertext;
+    [SerializeField] private TextMeshProUGUI CurrentChangetext;
     [SerializeField] private GameObject[] clientsImages;
 
-    private float[] numbersToRandomize;
+    private int[] changeNeeded;
+    private int currentClient = 0;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int selectedChange = 0;
+
+    private void Start()
     {
-        numbersToRandomize = new float[clientsImages.Length];
-        for (int i = 0; i < numbersToRandomize.Length; i++)
+        changeNeeded = new int[clientsImages.Length];
+
+        for (int i = 0; i < changeNeeded.Length; i++)
         {
-            numbersToRandomize[i] = Random.Range(1, 1000);
+            changeNeeded[i] = Random.Range(1, 1001); // cents: 1–1000
+            clientsImages[i].SetActive(i == 0);
+        }
+
+        ShowCurrentClient();
+    }
+
+    private void ShowCurrentClient()
+    {
+        selectedChange = 0;
+
+        if (currentClient >= clientsImages.Length)
+        {
+            CurrentChangetext.text = "All clients served!";
+            return;
+        }
+
+        Cashiertext.text = $"{changeNeeded[currentClient] / 100f:0.00}\n";
+    }
+
+    // Assign this from each UI button.
+    // Example: penny = 1, nickel = 5, dime = 10, dollar = 100.
+    //public void SubmitChange(int valueInCents)
+    public void SubmitChange()
+    {
+        if (currentClient >= clientsImages.Length)
+            return;
+
+        if (selectedChange == changeNeeded[currentClient])
+        {
+            clientsImages[currentClient].SetActive(false);
+            currentClient++;
+            ShowCurrentClient();
+        }
+        else if (selectedChange > changeNeeded[currentClient])
+        {
+            CurrentChangetext.text = "Too much change! Try again.";
+            selectedChange = 0;
+        }
+        else
+        {
+            Cashiertext.text = $"{changeNeeded[currentClient] / 100f:0.00}";
+                        
         }
     }
 
-    private void StartGame()
+    public void AddChange(int amount)
     {
-        text.text = numbersToRandomize.ToString();
+        selectedChange += amount;
+        CurrentChangetext.text = $"{selectedChange / 100f:0.00}";
     }
 
-    public void SubmitChange(int number_chosen)
+    public void ClearSelectedChange()
     {
-        
+        selectedChange = 0;
+        ShowCurrentClient();
     }
 }
